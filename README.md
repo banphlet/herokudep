e1701957f0462e51fe9bc9ef805982ba67d99181
Nana Kwame Zoe <banphlet4@gmail.com>
2019-10-06 21:34:37 +0000

    initial-commit

---
+++ README.md
@@ -0,0 +1,49 @@
+@strellio/heroku-dep
+====================
+
+Simple Cli for managing heroku deployments
+
+[![oclif](https://img.shields.io/badge/cli-oclif-brightgreen.svg)](https://oclif.io)
+[![Version](https://img.shields.io/npm/v/@strellio/heroku-dep.svg)](https://npmjs.org/package/@strellio/heroku-dep)
+[![Downloads/week](https://img.shields.io/npm/dw/@strellio/heroku-dep.svg)](https://npmjs.org/package/@strellio/heroku-dep)
+[![License](https://img.shields.io/npm/l/@strellio/heroku-dep.svg)](https://github.com/banphlet/heroku-dep/blob/master/package.json)
+
+<!-- toc -->
+* [Usage](#usage)
+* [Commands](#commands)
+<!-- tocstop -->
+# Usage
+<!-- usage -->
+```sh-session
+$ npm install -g @strellio/heroku-dep
+$ heroku-deploy COMMAND
+running command...
+$ heroku-deploy (-v|--version|version)
+@strellio/heroku-dep/0.0.0 darwin-x64 node-v8.16.1
+$ heroku-deploy --help [COMMAND]
+USAGE
+  $ heroku-deploy COMMAND
+...
+```
+<!-- usagestop -->
+# Commands
+<!-- commands -->
+* [`heroku-deploy help [COMMAND]`](#heroku-deploy-help-command)
+
+## `heroku-deploy help [COMMAND]`
+
+display help for heroku-deploy
+
+```
+USAGE
+  $ heroku-deploy help [COMMAND]
+
+ARGUMENTS
+  COMMAND  command to show help for
+
+OPTIONS
+  --all  see all commands in CLI
+```
+
+_See code: [@oclif/plugin-help](https://github.com/oclif/plugin-help/blob/v2.2.1/src/commands/help.ts)_
+<!-- commandsstop -->
---
+++ src/commands/deploy.ts
@@ -0,0 +1,51 @@
+#!/usr/bin/env node
+
+import { Command, flags } from "@heroku-cli/command"
+import * as Heroku from '@heroku-cli/schema'
+const debug = require("debug")("@strellio/dep-heroku")
+const simpleGit = require("simple-git/promise")().outputHandler((_: any, stdout: any, stderr: any) => {
+  stdout.pipe(process.stdout);
+  stderr.pipe(process.stderr);
+})
+import { formHerokuGitUrl } from "../../lib/utils";
+import { checkApplicationHealth, rollbackDeployment } from "../../lib/requests";
+
+
+
+class StrellioDepHeroku extends Command {
+  static description = 'Deploy heroku applications using one command'
+
+  static flags = {
+    // add --version flag to shocw CLI version
+    version: flags.version({ char: 'v' }),
+    help: flags.help({ char: 'h' }),
+    remote: flags.remote(),
+    app: flags.app({ required: true }),
+    token: flags.app({ required: true, description: "Heroku api token" })
+  }
+
+  rollbackDeployment = async (error: any) => {
+    const { flags } = this.parse(StrellioDepHeroku)
+    this.warn(`Deployment failed with statusCode ${error.statusCode}.Rolling back`)
+    await rollbackDeployment(this.heroku, flags.app)
+    this.log("rollback completed")
+  }
+
+  async run() {
+    const { flags } = this.parse(StrellioDepHeroku)
+    process.env.HEROKU_API_KEY = flags.token
+    debug("Testing heroku api key")
+    await this.heroku.get<Heroku.App>(`/apps/${flags.app}`).catch(() => this.error("Invalid HEROKU_API_KEY"))
+
+    debug("Deploying heroku application")
+    const isGitRepo = await simpleGit.checkIsRepo()
+    if (!isGitRepo) return this.error("Git not initialized.")
+
+    debug("Push to heroku master")
+    this.log("Deploying to application")
+    await simpleGit.push(formHerokuGitUrl(flags.token, flags.app), "master").catch((error: any) => this.error(`Error pushing to heroku ${error.message}`))
+    return checkApplicationHealth(flags.app).then(() => this.log("Deployment to heroku succeeded :)")).catch(this.rollbackDeployment)
+  }
+}
+
+export = StrellioDepHeroku
